bc6160b981d426041c9b0c1f96524ea2fedfdba2
Luis Benitez <luisbenitez02@gmail.com>
2015-06-09 12:53:54 -0500

    Random v 1.0
    Primera version del proyecto Random de MiriadaX como ejemplo de Github

---
+++ random.js
@@ -0,0 +1,7 @@
+//ejemplo para MiriadaX Modulo 5 github
+var numero = Math.random()
+if (numero <= 0.5){
+  console.log('El numero es menor que 0.5')
+}else {
+  console.log('El numero es mayor que 0.5')
+}
